4a57f7a59b576df17e73a78b46136332d71f2255
Austin Jones <ajj29@pitt.edu>
2017-02-27 15:36:37 -0500

    Revert "Allowed server to bind to more than localhost"
    This reverts commit 66923b05ffe7ceef73eee881f50582da1425e792.

--- src/edu/pitt/battleshipgame/endpoint/ServerPublisher.java
+++ src/edu/pitt/battleshipgame/endpoint/ServerPublisher.java
@@ -1,24 +1,12 @@
 package edu.pitt.battleshipgame.endpoint;
 
 import javax.xml.ws.Endpoint;
-import java.util.Scanner;
+
 import edu.pitt.battleshipgame.server.ServerWrapper;
 
 //Endpoint publisher
 public class ServerPublisher {
     public static void main(String [] args) {
-	while(true){
-            try{
-	        System.out.println("Enter address to bind to (empty for 'http://localhost:9999/battleship'):");
-	        Scanner sc = new Scanner(System.in);
-	        String host = sc.nextLine();
-	        if(host.equals(""))
-	            host = "http://localhost:9999/battleship";
-                Endpoint.publish(host, new ServerWrapper());
-		break;
-	    }catch(Exception e){
-	        System.out.println(e);
-	    }
-	}
+        Endpoint.publish("http://localhost:9999/battleship", new ServerWrapper());
     }
 }
