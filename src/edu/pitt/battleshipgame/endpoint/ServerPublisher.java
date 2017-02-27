66923b05ffe7ceef73eee881f50582da1425e792
Alex Pickering <amp215@pitt.edu>
2017-02-27 13:16:25 -0500

    Allowed server to bind to more than localhost

--- src/edu/pitt/battleshipgame/endpoint/ServerPublisher.java
+++ src/edu/pitt/battleshipgame/endpoint/ServerPublisher.java
@@ -1,12 +1,24 @@
 package edu.pitt.battleshipgame.endpoint;
 
 import javax.xml.ws.Endpoint;
-
+import java.util.Scanner;
 import edu.pitt.battleshipgame.server.ServerWrapper;
 
 //Endpoint publisher
 public class ServerPublisher {
     public static void main(String [] args) {
-        Endpoint.publish("http://localhost:9999/battleship", new ServerWrapper());
+	while(true){
+            try{
+	        System.out.println("Enter address to bind to (empty for 'http://localhost:9999/battleship'):");
+	        Scanner sc = new Scanner(System.in);
+	        String host = sc.nextLine();
+	        if(host.equals(""))
+	            host = "http://localhost:9999/battleship";
+                Endpoint.publish(host, new ServerWrapper());
+		break;
+	    }catch(Exception e){
+	        System.out.println(e);
+	    }
+	}
     }
 }
